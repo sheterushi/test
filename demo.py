b61cbdb330d6de5d52f82342145969c55be534ce
sheterushi <84314173+sheterushi@users.noreply.github.com>
2023-01-07 11:50:35 +0530

    Delete demo.py

--- demo.py
+++
@@ -1,7 +0,0 @@
-print("Hello, We are in the demo session of git")
-
-n1 = int(input("Enter the first no:"))
-n2 = int(input("Enter the first no:"))
-print("Addition of numbers is:",n1+n2)
-print("Substraction of numbers is:",n1-n2)
-print("Multiplication of numbers is:",n1*n2)
